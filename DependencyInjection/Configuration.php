7aa0d4e75ff53a1bfcc2f47b41fada7a9fbb753b
Thomas Beaujean <thomas.beaujean@hotmail.fr>
2024-02-23 09:00:49 +0100

    fix interface compatibility

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -7,7 +7,7 @@ use Symfony\Component\Config\Definition\ConfigurationInterface;
 
 class Configuration implements ConfigurationInterface
 {
-    public function getConfigTreeBuilder()
+    public function getConfigTreeBuilder(): TreeBuilder
     {
         $treeBuilder = new TreeBuilder('query_builder_repository_generator');
         $rootNode = $treeBuilder->getRootNode();
